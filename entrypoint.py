dc5dadd14f6843cad26e1093d20cbbcd59770689
sscalvo <zergios@yahoo.com>
2020-09-26 13:44:15 +0200

    cleaning

--- entrypoint.py
+++ entrypoint.py
@@ -2,8 +2,6 @@
 # 05/08/2020
 import os
 from app import create_app
-#app = create_app()
-# settings_module = os.getenv('APP_SETTINGS_MODULE')  #comentada para Heroku
-# app = create_app(settings_module)  # comentada para Heroku
-app = create_app() # default value for setting_module is 'config.prod'. This way we dont need environ vars in Heroku
+settings_module = os.getenv('APP_SETTINGS_MODULE')  #
+app = create_app(settings_module)  # default value for setting_module is 'config.prod'
 
--- environs.sh
+++
@@ -1,4 +0,0 @@
-export FLASK_APP=entrypoint.py
-export FLASK_ENV=development
-export APP_SETTINGS_MODULE=config.local
-flask run
