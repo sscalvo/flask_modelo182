555962dc576e04e3c070ccb6961d2b46f3498cd1
sscalvo <zergios@yahoo.com>
2020-09-26 10:21:48 +0200

    tunning and generalizing config

--- config/default.py
+++ config/default.py
@@ -8,7 +8,7 @@ BASE_DIR           = dirname(dirname(abspath(__file__)))
 DIR_STATIC         = os.path.join(get_root_path('app.public'), 'static' ) 
 DIR_DOWNLOADS      = os.path.join(get_root_path('app.public'), 'downloads' ) 
 
-SECRET_KEY         = 'fnskdfji498334ji34jkj43jk5j43jk54j35kj234h5kj3h45jnfjknsdjkcal'
+SECRET_KEY         = 'your secret key'
 ALLOWED_EXTENSIONS = ['txt', 'csv', 'tsv']
 MAX_CONTENT_LENGTH = 16 * 1024 * 1024 # max file-upload size= 16 Mb
 
@@ -17,18 +17,18 @@ MAX_CONTENT_LENGTH = 16 * 1024 * 1024 # max file-upload size= 16 Mb
 MAIL_SERVER            = 'smtp.gmail.com'
 MAIL_PORT              = 587
 MAIL_USE_TLS           = True
-MAIL_USERNAME          = "fakemail@gmail.com" 
+MAIL_USERNAME          = "email address from where app will be sendig emails"
 MAIL_USE_SSL           = False
-MAIL_PASSWORD          = "fakepass"
-MAIL_DEFAULT_SENDER    = 'appmodelo182@gmail.com'
+MAIL_PASSWORD          = "password for MAIL_USERNAME address"
+MAIL_DEFAULT_SENDER    = 'email address shown as sender'
 MAIL_SUPRESS_SEND      = False
 MAIL_MAX_EMAILS        = None
 MAIL_ASCII_ATTACHMENTS = False
 
 
 
-SEND_TO_ADDRESS        = 'sscalvo@gmail.com'
-AUTHOR_STR             = 'sscalvo' #einasai
+SEND_TO_ADDRESS        = 'email address that will receive emails (you may want this to be your own email)'
+AUTHOR_STR             = 'sscalvo'
 AUTHOR_WEB             = 'http://www.sscalvo.com'
 # App environments
 APP_ENV_LOCAL = 'local'
